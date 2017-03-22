046ebdfbab17690c0b4f3c5c1ea31ca3f43abab9
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-03-22 19:29:32 +0100

    Upgrade hindent

--- thesis/src/Thesis/Utils.hs
+++ thesis/src/Thesis/Utils.hs
@@ -5,9 +5,7 @@ module Thesis.Utils where
 import Import
 
 -- TODO make it also possible to do this with absolute paths.
-(<.>)
-    :: MonadThrow m
-    => Path Rel File -> String -> m (Path Rel File)
+(<.>) :: MonadThrow m => Path Rel File -> String -> m (Path Rel File)
 (<.>) f e = parseRelFile $ toFilePath f ++ "." ++ e
 
 tmpDir :: Path Rel Dir
