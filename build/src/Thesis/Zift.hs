e092bdd11bcc2eb3c40e5e6d4c96fd1e295832b8
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-04-05 00:15:06 +0200

    Much better zift test than running shake in the current directory

--- build/src/Thesis/Zift.hs
+++ build/src/Thesis/Zift.hs
@@ -4,22 +4,17 @@ module Thesis.Zift
 
 import Import
 
-import System.Environment (withArgs)
-
 import Development.Shake
-import Development.Shake.Path
 
 import Zifter
 
-import Thesis.Document
-import Thesis.ShakeBuild
-
 thesisZiftScript :: ZiftScript ()
-thesisZiftScript = do
-    preprocessor $ pure ()
-    checker $
-        liftIO $
-        withArgs [] $
-        shakeArgs shakeOptions $ do
-            thesisShakeBuildRules
-            wantP [thesisOut]
+thesisZiftScript =
+    checker $ do
+        rd <- getRootDir
+        Stdout out1 <-
+            liftIO $ cmd (Cwd $ toFilePath rd) "stack" "install" ":thesis"
+        printZift out1
+        -- This automatically runs it in the weird tmp dir, so we make sure that it works anywhere.
+        Stdout out2 <- liftIO $ cmd "thesis" "build"
+        printZift out2
