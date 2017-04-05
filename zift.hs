94ab74970276e2e06ec5bdb58f5d36c232c4c59c
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-04-05 10:18:20 +0200

    And again

--- zift.hs
+++ zift.hs
@@ -29,7 +29,9 @@ main =
         prechecker gitAddAllZift
         checker $ do
             do rd <- getRootDir
-               Stdout out1 <- liftIO $ cmd (Cwd $ toFilePath rd) "stack" "install"
+               Stdout out1 <-
+                   liftIO $
+                   cmd (Cwd $ toFilePath rd) "stack" "install" ":thesis"
                printZift out1
                 -- This automatically runs it in the weird tmp dir, so we make sure that it works anywhere.
                Stdout out2 <-
