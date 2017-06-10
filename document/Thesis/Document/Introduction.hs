084b8c191dd77e0ad9b99b424029abedd7b5d204
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-06-10 08:43:58 +0200

    Little fixes

--- document/Thesis/Document/Introduction.hs
+++ document/Thesis/Document/Introduction.hs
@@ -26,7 +26,7 @@ thesisIntroduction =
                     "In practice, formal methods are usually too expensive and mostly not feasible."
                 citationNeeded
             subsubsection "Unit Testing" $ do
-                s "Unit testing is an initial approach to the testing problem."
+                s "Unit testing is a widely used approach to testing."
                 s
                     "A unit test consists of a piece of code, without arguments, and possibly some assertions about the results."
                 s
@@ -43,7 +43,6 @@ thesisIntroduction =
                         "Indeed, unit tests often fail to address all possible aspects of the code under test."
                     s
                         "One would have to write at least one unit test for every possible code path of the code under test to have a chance at covering all possible aspects of the code under test."
-                    s "This brings us to the next problem."
                 paragraph "Developer-time Costs" $ do
                     s
                         "Unit testing, but testing in general, is quite expensive when it comes to developer time."
@@ -63,12 +62,12 @@ thesisIntroduction =
                 s
                     "The code now tests a property of this argument, namely the property that the test passes when given this input."
                 s
-                    "We say that the property test passes if it passes for a given number of arbitrarily chosen input values."
+                    "We say that the property test passes if it passes for a given number of randomly chosen input values."
                 lnbk
                 s
                     "Property testing solves the coverage problem of unit testing probabilistically, but exacerbates the second problem of unit testing."
                 s
-                    "As the number of executions of a property test increases, the probability of covering all possible aspects of the code under test should tend to one."
+                    "As the number of executions of a property test increases, the probability to cover all possible aspects of the code under test should tend to one."
                 s
                     "This means that a programmer now only needs to write a limited number of property tests, instead of a large number of unit tests."
                 s
