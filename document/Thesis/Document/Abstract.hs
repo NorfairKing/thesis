6340b2eb5f8023b6bbd961e60fec5caeea3f43b7
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-08-09 09:52:11 +0200

    First draft of an abstract

--- document/Thesis/Document/Abstract.hs
+++ document/Thesis/Document/Abstract.hs
@@ -9,5 +9,24 @@ import DocImport
 thesisAbstract :: Thesis
 thesisAbstract = do
     newpage
-    abstract
-        "We contribute a technique that can make property discovery practically feasible by taming the complexity"
+    abstract $ do
+        s
+            "We contribute a technique that can make property discovery practically feasible by taming the complexity of the discovery algorithm."
+        lnbk
+        -- Motivation
+        s
+            "Property discovery has the potential to be a great tool for practical software correctness."
+        -- Problem statement
+        s
+            "Unfortunately current methods remain infeasible because of the complexity of the discovery algorithm."
+        -- Approach
+        s
+            "We contribute a new approach to taming the complexity of the discovery algorithm that runs the discovery algorithm multiple times with intelligently chosen inputs of smaller size: signature inference."
+        -- Results
+        s
+            "The results suggest that this approach is multiple orders of magnitude faster, fast enough to be practical, and does not sacrifice any results."
+        s
+            "In fact, this approach improves upon the current property discovery algorithm with respect to results as well."
+        -- Conclusions
+        s
+            "In conclusion, this contribution has made property discovery a feasible tool for use in practical software development."
