4f7265797a79c60b161a43be47dcfa2d4b1efe77
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-08-23 18:30:25 +0200

    fixed the acknowledgments

--- document/Thesis/Document/Acknowledgements.hs
+++ document/Thesis/Document/Acknowledgements.hs
@@ -12,21 +12,22 @@ thesisAcknowledgements =
         raw "\n"
         center $ large $ textbf "Acknowledgements"
         raw "\n"
-        s "I would first like to thank my thesis adviser Dr Dmitriy Traytel."
+        s "I would first like to acknowledge Professor David Basin."
+        s
+            "He allowed me to work on a topic of my choosing and believed in me when I did."
+        lnbk
+        s "I would also like to thank my thesis adviser Dr Dmitriy Traytel."
         s
             "His door was always open whenever I ran into a trouble spot or had a question about my research or writing."
         s
             "He consistently allowed this work to be my own, but steered me in the right the direction whenever he thought I needed it."
         lnbk
         s
-            "I would also like to acknowledge Professor David Basin as the second reader of this thesis, and I am gratefully indebted to him for his very valuable comments on this thesis."
-        lnbk
-        s
             "Finally, I must express my very profound gratitude to my parents and to my brother for providing me with unfailing support and continuous encouragement throughout my years of study and through the process of researching and writing this thesis."
         s "This accomplishment would not have been possible without them."
         lnbk
         raw
-            "As such, I would like to dedicate this work to my father, and introduce this work with his summary of a thesis:"
+            "As such, I would like to dedicate this work to my father, and introduce this work with his summary of what it means to present a thesis:"
         center $
             raw
                 "Ik heb ergens op gestudeerd en nu ga ik mijn vertellingske doen."
