afa4c07b5f2927b4655e6b00ebd16481b8abf096
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-08-29 09:01:10 +0200

    rewrote the discussion section

--- document/Thesis/Document/Discussion.hs
+++ document/Thesis/Document/Discussion.hs
@@ -18,6 +18,9 @@ thesisDiscussion =
             m_ = "M"
             f_ = "F"
             i_ = "i"
+            j_ = "j"
+            dc_ = "DC"
+            dc_' = "DC'"
         hereFigure $ do
             let fmt =
                     [ VerticalLine
@@ -25,24 +28,45 @@ thesisDiscussion =
                     , VerticalLine
                     , LeftColumn
                     , VerticalLine
+                    , LeftColumn
+                    , VerticalLine
                     ]
             let header = do
                     hline
-                    "Strategy" & "Discovery Complexity"
+                    "Strategy" & m dc_ & m dc_' -- Discovery Complexity"
                     lnbk
                     hline
                     hline
             tabular Nothing fmt $ do
+                let syntDC = i_ ^: (2 * m_)
+                    chunDC = f_ * i_ * 2 ^: (2 * m_)
+                    chunPDC = f_ * (i_ ^: 2) * 3 ^: (2 * m_)
                 header
                 mapM_
-                    (\(strat, compl) -> strat & m (bigoh compl) >> lnbk >> hline)
-                    [ (fullBackground, s_ ^: (2 * m_))
-                    , (emptyBackground, f_)
-                    , (syntacticSimilarityName, i_)
-                    , (syntacticSimilaritySymbols, i_)
-                    , (syntacticSimilarityType, i_)
-                    , (chunks, s_)
-                    , (chunksPlus, s_ ^: 2)
+                    (\(strat, compl, compl2) ->
+                         strat & m (bigoh compl) & m (bigoh compl2) >> lnbk >>
+                         hline)
+                    [ (fullBackground, s_ ^: (2 * m_), s_ ^: 14)
+                    , (emptyBackground, f_ ^: (2 * m_), 1)
+                    , (syntacticSimilarityName, syntDC, 1)
+                    , (syntacticSimilaritySymbols, syntDC, 1)
+                    , (syntacticSimilarityType, syntDC, 1)
+                    , (typeReachability, s_ ^: (2 * m_), s_ ^: 14)
+                    , (chunks, s_ * f_ * 2 ^: (2 * m_), s_)
+                    , (chunksPlus, (s_ ^: 2) * f_ * 3 ^: (2 * m_), s_ ^: 2)
+                    , (chunksSimilarityName, chunDC, 1)
+                    , (chunksSimilaritySymbols, chunDC, 1)
+                    , (chunksSimilarityType, chunDC, 1)
+                    , (chunksTypeReachability, s_ * f_ * 2 ^: (2 * m_), s_)
+                    , (chunksPlusSimilarityName, chunPDC, 1)
+                    , (chunksPlusSimilaritySymbols, chunPDC, 1)
+                    , (chunksPlusSimilarityType, chunPDC, 1)
+                    , ( chunksPlusTypeReachability
+                      , (s_ ^: 2) * f_ * 3 ^: (2 * m_)
+                      , s_ ^: 2)
+                    , (chunksPlusReachabilityName, chunPDC, 1)
+                    , (chunksPlusReachabilitySymbols, chunPDC, 1)
+                    , (chunksPlusReachabilityType, chunPDC, 1)
                     ]
             lab summaryLabel
             caption "A summary of the different signature inference strategies"
@@ -55,6 +79,23 @@ thesisDiscussion =
             sym
                 i_
                 "chosen size of the signature in a reducing signature inference strategy"
+            sym j_ "chosen depth of type reachability"
+            sym dc_ "worst case discovery complexity"
+            sym dc_' $ do
+                sequence_
+                    [ "worst case discovery complexity, when "
+                    , m m_
+                    , " is fixed to be "
+                    , m 7 <> ", "
+                    , m i_
+                    , " is fixed to be "
+                    , m 5
+                    , " and "
+                    , m j_
+                    , " is fixed to be "
+                    , m 7
+                    ]
+                s "This is the configuration that we chose."
         subsection "Configurability" $ do
             s
                 "The research into different signature inference strategies has had the nice side effect that signature inference can now be configured accordingly."
@@ -64,17 +105,15 @@ thesisDiscussion =
             s
                 "One potential user interface involves online property discovery and immediate feedback."
             s
-                "This use case can be enabled by choosing a signature inference strategy that runs in constant time and space, and would then work on almost any code base."
+                "This use case can be enabled by choosing a signature inference strategy that has an appropriate discovery complexity."
             lnbk
             s
                 "Another potential user interface involves running property discovery over night."
             s
                 "In this case there is a larger number of feasible signature inference strategies available."
-            l
-                [ "For best results, a strategy that runs in linear or logarithmic time can be chosen, such as"
-                , chunks
-                ]
-            todo "what else?"
+            lnbk
+            s
+                "Users are not forced to use any single signature inference strategy, and are invited to choose their own and evaluate whether it is appropriate for their use case."
         subsection "Shortcomings" $ do
             s
                 "EasySpec suffers from most of the functional shortcomings that QuickSpec has."
@@ -84,8 +123,22 @@ thesisDiscussion =
                 "This is not a big problem since a human must still select the properties that they want to have hold, and the properties are still tested afterwards."
             lnbk
             s
-                "EasySpec can only discover properties that already hold (modulo false positives)."
+                "Both QuickSpec and EasySpec can only discover properties that already hold (modulo false positives)."
             s
                 "This means that properties that you may want to have hold about code will not be discovered if the code does not already satisfy those properties."
-            todo "what else?"
-        todo "what else?"
+            lnbk
+            s
+                "Higher kinded type variables are not supported in EasySpec because their monomorphisation is still done manually in QuickSpec."
+            lnbk
+            todo "refer to the section where I explained this problem earlier"
+            l
+                [ "Both QuickSpec and EasySpec use the"
+                , haskInline "arbitrary"
+                , "generators from the"
+                , haskInline "Arbitrary"
+                , "type class to generate random values"
+                ]
+            s
+                "Custom generators could be of great value if certain properties only hold for a subset of a type, but neither QuickSpec nor EasySpec currently supports them."
+            s
+                "Furthermore, EasySpec currently does not find, and inform QuickSpec about, instances that are in scope, so EasySpec will only operate on types of which QuickSpec already has the Arbitrary instance."
